b9de59ba422ffa30b30f19ee7351040b9b69e87c
radharukminiY <rukku558@gmail.com>
2021-04-23 21:22:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,180 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import firebase from 'firebase/app';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from "./components/Header";
+import Form from "./components/Form";
+import Notes from "./components/Notes";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      notes: [],
+      filterTag: "",
+      filterDate: "",
+      filteredNotes: [],
+      filter: false
+    };
+  }
+
+  // initialise database object
+  componentDidMount() {
+    this.db = firebase.database();
+    this.listenForChange();
+  }
+
+  // update the notes array on deletion or insertion or updation of a note
+  listenForChange() {
+    this.db.ref("notes").on("child_added", snapshot => {
+      let note = {
+        id: snapshot.key,
+        title: snapshot.val().title,
+        note: snapshot.val().note,
+        tag: snapshot.val().tag,
+        created_at: snapshot.val().created_at
+      };
+
+      let notes = this.state.notes;
+      notes.push(note);
+
+      this.setState({
+        notes: notes
+      });
+    });
+
+    this.db.ref("notes").on("child_changed", snapshot => {
+      let notes = this.state.notes;
+      let index = notes.findIndex(note => note.id === snapshot.key);
+      notes[index].title = snapshot.val().title;
+      notes[index].note = snapshot.val().note;
+      notes[index].tag = snapshot.val().tag;
+
+      this.setState({
+        notes: notes
+      });
+    });
+
+    this.db.ref("notes").on("child_removed", snapshot => {
+      let notes = this.state.notes;
+      notes = notes.filter(note => note.id !== snapshot.key);
+
+      this.setState({
+        notes: notes
+      });
+    });
+  }
+
+  onChangeHandler(evt, key) {
+    this.setState({
+      [key]: evt.target.value
+    });
+  }
+
+  filterNoteByTag() {
+    if (this.state.filterTag !== "") {
+      let notes;
+      if (this.state.filteredNotes.length !== 0)
+        notes = this.state.filteredNotes;
+      else notes = this.state.notes;
+      let newNotes = notes.filter(note => note.tag === this.state.filterTag);
+      this.setState({
+        filteredNotes: newNotes,
+        filter: true
+      });
+    }
+  }
+
+  filterNoteByDate() {
+    if (this.state.filterDate !== "") {
+      let date = this.state.filterDate;
+      let yy = date.substr(0, 4);
+      let mm = date.substr(5, 2);
+      let dd = date.substr(8, 2);
+      let newDate = dd + "/" + mm + "/" + yy;
+
+      let notes;
+      if (this.state.filteredNotes.length !== 0)
+        notes = this.state.filteredNotes;
+      else notes = this.state.notes;
+
+      let newNotes = notes.filter(note => note.created_at.includes(newDate));
+      this.setState({
+        filteredNotes: newNotes,
+        filter: true
+      });
+    }
+  }
+
+  clearFilters() {
+    if (this.state.filter) {
+      this.setState({
+        filteredNotes: [],
+        filterTag: "",
+        filterDate: "",
+        filter: false
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <main>
+          <Form />
+          <hr></hr>
+          <section className="noteform-filter">
+            <h3>Search Notes</h3>
+            <div className="form-flex">
+              <span className="form-group">
+                <label htmlFor="noteform-tag">Filter by Tag</label>
+                &nbsp;
+                <input
+                  type="text"
+                  id="noteform-tag"
+                  name="noteform-tag"
+                  value={this.state.filterTag}
+                  onChange={evt => this.onChangeHandler(evt, "filterTag")}
+                />
+                &nbsp;
+                <button onClick={() => this.filterNoteByTag()}>
+                  Filter Notes
+                </button>
+              </span>
+
+              <span className="form-group">
+                <button onClick={() => this.clearFilters()}>
+                  Clear Filters
+                </button>
+              </span>
+
+              <span className="form-group">
+                <label htmlFor="noteform-date">Filter by Date</label>
+                &nbsp;
+                <input
+                  type="date"
+                  id="noteform-date"
+                  name="noteform-date"
+                  value={this.state.filterDate}
+                  onChange={evt => this.onChangeHandler(evt, "filterDate")}
+                />
+                &nbsp;
+                <button onClick={() => this.filterNoteByDate()}>
+                  Filter Notes
+                </button>
+              </span>
+            </div>
+          </section>
+          {this.state.filter ? (
+            <Notes notes={this.state.filteredNotes} />
+          ) : (
+            <Notes notes={this.state.notes} />
+          )}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase/app';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const firebaseConfig = {
+  apiKey: "AIzaSyC1tyI9Wbdp5zhy9qXl0pQyjOkd0rvk3Lg",
+  authDomain: "todo-list-40a32.firebaseapp.com",
+  databaseURL: "https://todo-list-40a32-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "todo-list-40a32",
+  storageBucket: "todo-list-40a32.appspot.com",
+  messagingSenderId: "1005969185406",
+  appId: "1:1005969185406:web:2ff355b5f500983478d62f",
+  measurementId: "G-CN4T3B8H59"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
